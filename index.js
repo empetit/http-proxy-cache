338a07c7de28a6df1dab48873f6c2596dfdb3c88
DESKTOP-S6G8FVK\epeti <epetit11@gmail.com>
2017-04-23 15:06:11 +0200

    Added CORS

--- index.js
+++ index.js
@@ -65,6 +65,12 @@ var cache = (duration) => {
     }
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 // --------------------------------
 // ROUTES
